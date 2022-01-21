d6af1074423704adc9bd1ff6b6c6b6dea3d5d227
Tung Pham <tungpham.bh@gmail.com>
2022-01-21 11:24:10 +0700

    add file main.js chua ham removeRow

---
+++ public/template/admin/js/main.js
@@ -0,0 +1,24 @@
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+function removeRow(id, url){
+    if(confirm('Ban chac khong?')){
+        $.ajax({
+            type: 'DELETE',
+            datatype: 'JSON',
+            url: url,
+            data: { id },
+            url: url,
+            success: function (data) {
+                console.log(data);
+                alert(data.message);
+                if(data.error === false){
+                     location.reload();
+                }
+            }
+        });
+    }
+}
